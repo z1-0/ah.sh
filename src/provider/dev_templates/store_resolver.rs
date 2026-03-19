1fcb2c42d98c8f71f400a063e334f1ffb4b53202
zion <ox7a696f6e@outlook.com>
2026-03-20 07:59:55 +0800

    chore(tests): reduce resolver fake runner type complexity

--- src/provider/dev_templates/store_resolver.rs
+++ src/provider/dev_templates/store_resolver.rs
@@ -141,10 +141,12 @@ mod tests {
         output: std::result::Result<String, String>,
     }
 
+    type ProgramArgsCall = (String, Vec<String>);
+
     #[derive(Clone, Debug)]
     struct FakeRunner {
         replies: Arc<Mutex<VecDeque<CommandReply>>>,
-        calls: Arc<Mutex<Vec<(String, Vec<String>)>>>,
+        calls: Arc<Mutex<Vec<ProgramArgsCall>>>,
     }
 
     impl FakeRunner {
